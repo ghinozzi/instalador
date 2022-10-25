88c51110b18ad09586fc0a478f8ead6656778452
João Pedro Ghinozzi <ghinozzi@gmail.com>
2022-10-25 10:31:12 -0400

    view instalador

--- app/Http/Controllers/InstaladorController.php
+++ app/Http/Controllers/InstaladorController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class InstaladorController extends Controller
 {
     public function index(){
 
+        $tabelas = DB::select('show tables;');
 
-        return view('instalador.index');
+        return view('instalador.index',compact('tabelas'));
     }
 }
--- resources/views/instalador/index.blade.php
+++ resources/views/instalador/index.blade.php
@@ -14,72 +14,79 @@
                     </div>
                 </div>
                 <div class="accordion" id="accordionExample">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingOne">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Accordion Item #1
-                            </button>
-                        </h2>
-                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                            data-bs-parent="#accordionExample">
+                    @foreach($tabelas as $tabela)
+                    @php
+                        $array = (array)$tabela;
+                        $tabela = $array[array_key_first($array)];
+                    @endphp
+                    <div class="accordion-item instalador-accordion">
+                        <div class="row">
+                            <div class="col-md-3 instalador-item"><a  data-bs-toggle="collapse" data-bs-target="#{{$tabela}}">{{$tabela}}</a></div>
+                            <div class="col-md-9">
+                                <div class='instalador-options'>
+                                    <input type="checkbox" > <label for="">Model</label>
+                                    <input type="checkbox" > <label for="">Controller</label>
+                                    <input type="checkbox" > <label for="">View</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="{{$tabela}}" class="accordion-collapse collapse" >
                             <div class="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the
-                                collapse
-                                plugin adds the appropriate classes that we use to style each element. These classes control
-                                the overall
-                                appearance, as well as the showing and hiding via CSS transitions. You can modify any of
-                                this with
-                                custom CSS or overriding our default variables. It's also worth noting that just about any
-                                HTML can go
-                                within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <p><strong>Campos</strong></p>
+                                <table class="table">
+                                    <tr>
+                                        <th>Campo</th>
+                                        <th>Tipo</th>
+                                        <th>Relação</th>
+                                        <th>Campo Relação</th>
+                                    </tr>
+                                    <!--foreach campos-->
+                                    <tr>
+                                        <td>Nome</th>
+                                        <td>
+                                            <select name="" id="">
+                                                <option value="">Selecione</option>
+                                                <option value="">Texto</option>
+                                                <option value="">Data</option>
+                                                <option value="">Texto Longo</option>
+                                                <option value="">Editor Html</option>
+                                                <option value="">Select</option>
+                                            </select>
+                                        </th>
+                                        <td></th>
+                                        <td></th>
+                                    </tr>
+                                    <tr>
+                                        <td>Tipo</th>
+                                        <td>
+                                            <select name="" id="">
+                                                <option value="">Selecione</option>
+                                                <option value="">Texto</option>
+                                                <option value="">Data</option>
+                                                <option value="">Texto Longo</option>
+                                                <option value="">Editor Html</option>
+                                                <option value="">Select</option>
+                                            </select>
+                                        </th>
+                                        <td>
+                                            <select name="" id="">
+                                                <option value="">tabela vinculada 1</option>
+                                                <option value="">tabela vinculada 2</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select name="" id="">
+                                                <option value="">Campo tabela vinculada 1</option>
+                                            </select>
+                                        </td>
+                                    </tr>
+                                    <!-- endfoeach foreach campos-->
+
+                                </table>
                             </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingTwo">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Accordion Item #2
-                            </button>
-                        </h2>
-                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                            data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until
-                                the collapse
-                                plugin adds the appropriate classes that we use to style each element. These classes control
-                                the overall
-                                appearance, as well as the showing and hiding via CSS transitions. You can modify any of
-                                this with
-                                custom CSS or overriding our default variables. It's also worth noting that just about any
-                                HTML can go
-                                within the <code>.accordion-body</code>, though the transition does limit overflow.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingThree">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Accordion Item #3
-                            </button>
-                        </h2>
-                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
-                            data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
-                                collapse
-                                plugin adds the appropriate classes that we use to style each element. These classes control
-                                the overall
-                                appearance, as well as the showing and hiding via CSS transitions. You can modify any of
-                                this with
-                                custom CSS or overriding our default variables. It's also worth noting that just about any
-                                HTML can go
-                                within the <code>.accordion-body</code>, though the transition does limit overflow.
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
